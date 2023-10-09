6628a20d670f6cb8b46abda69e508ed7848e7f33
kishorkumbhar <k.k.kumbhar007@hotmail.com>
2023-10-10 04:12:58 +0530

    new build

---
+++ src/main/java/com/ihub/ihub/Controller.java
@@ -0,0 +1,12 @@
+package com.ihub.ihub;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping ("/ihub")
+    public String index(){
+        return "Springboot Application iHUB-Demo :)";
+    }
+}
---
+++ src/main/java/com/ihub/ihub/IhubApplication.java
@@ -0,0 +1,13 @@
+package com.ihub.ihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhubApplication.class, args);
+	}
+
+}
