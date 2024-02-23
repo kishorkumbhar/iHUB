8f55da1f95193d65c948f159b2dc8950e62855ec
Kishor Kumbhar <55458154+kishorkumbhar@users.noreply.github.com>
2024-02-23 19:41:25 +0530

    Update Controller.java

--- src/main/java/com/ihub/ihub/Controller.java
+++ src/main/java/com/ihub/ihub/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping ("/")
+    @RequestMapping (/public")
     public String home(){
         return "index";
     }
