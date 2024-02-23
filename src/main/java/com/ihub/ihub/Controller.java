3b466e068a960b2228cea0326f714665bb1bf51b
Kishor Kumbhar <55458154+kishorkumbhar@users.noreply.github.com>
2024-02-23 21:29:03 +0530

    Update Controller.java

--- src/main/java/com/ihub/ihub/Controller.java
+++ src/main/java/com/ihub/ihub/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class Controller {
+public class HomeController {
     @RequestMapping ("/public")
     public String home(){
         return "index";
